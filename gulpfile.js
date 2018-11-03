047a9c735256f447c4c5ff983365910331048546
edely <edely.gomes@gmail.com>
2018-11-03 06:48:38 -0300

    Adiciona Bootstrap Via Npm; Concatena Estilos (sass)

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const jsmin = require('gulp-jsmin');
 const rename = require('gulp-rename');
+const uglifycss = require('gulp-uglifycss');
 
 gulp.task('default', ['sass', 'concat-css', 'js', 'compress']);
 //gulp.task('default', ['sass', 'concat-css', 'js', 'compress', 'clean', 'images', 'fonts']);
@@ -16,4 +17,12 @@ gulp.task('sass', function(){
     return gulp.src(['./alba/static/styles.scss'])
         .pipe(sass())
         .pipe(gulp.dest('./alba/static/'))
+});
+
+gulp.task('concat-css', ['sass'], function(){
+    return gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.min.css',  './alba/static/styles.css'])
+        .pipe(concat('styles.css'))
+        .pipe(uglifycss())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./alba/static/'));
 });
